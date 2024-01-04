a155f2d735a50436badb6f7962553d68d54f8fbf
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-04 14:03:29 +0200

    refactor function to handle new database model

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,6 +1,6 @@
 const validateResult = (query, result) => {
   const userWord = Object.values(query)[0].toLowerCase();
-  const correctWord = Object.values(result)[0].finnish.toLowerCase();
+  const correctWord = Object.values(result)[0].finnish_word.toLowerCase();
   if (userWord === correctWord) {
     return true;
   } else {
