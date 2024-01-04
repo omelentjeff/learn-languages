a6a088ad2c9f275e3f7bbc0f7800a8ec1476c39a
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-04 14:57:59 +0200

    feat: create route to add a new user

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,6 +1,7 @@
 const database = require("../database/database");
 const express = require("express");
 const userRouter = express.Router();
+const bcrypt = require("bcrypt");
 
 userRouter.get("/", async (req, res) => {
   try {
@@ -8,6 +9,31 @@ userRouter.get("/", async (req, res) => {
     res.json(users);
   } catch (err) {
     res.status(500).json({ msg: err });
+  }
+});
+
+userRouter.post("/", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    // Attempt to save the user
+    try {
+      const newUser = await database.saveUser(
+        req.body.username,
+        hashedPassword
+      );
+      res.status(201).json(newUser);
+    } catch (error) {
+      if (error.message === "Username is already taken") {
+        res.status(400).json({
+          msg: "Username is already taken. Please choose a different one.",
+        });
+      } else {
+        res.status(500).json({ msg: "Internal server error" });
+      }
+    }
+  } catch (err) {
+    res.status(500).json({ msg: err.message || "Internal server error" });
   }
 });
 
