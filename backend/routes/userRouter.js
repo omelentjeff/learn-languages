962287495c740095b4c5192743ccdee046622f43
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-04 14:30:34 +0200

    feat: create route to get all users

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -2,4 +2,13 @@ const database = require("../database/database");
 const express = require("express");
 const userRouter = express.Router();
 
+userRouter.get("/", async (req, res) => {
+  try {
+    const users = await database.findAllUsers();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
+});
+
 module.exports = userRouter;
