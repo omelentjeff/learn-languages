5b8419137728c7896ba32579633e7295021a1a25
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-06 14:31:47 +0200

    feat: add components and routes to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
-import { useState } from "react";
+// App.jsx
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
-function App() {}
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<SignIn />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
