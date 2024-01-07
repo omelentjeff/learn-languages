cdc62fcc3f7b4823688565653012b3b05e578411
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-07 15:18:35 +0200

    feat: modify route to get all languages and their word counts

--- backend/routes/languageRouter.js
+++ backend/routes/languageRouter.js
@@ -3,18 +3,11 @@ const { validateResult } = require("../utils/validation");
 const express = require("express");
 const languageRouter = express.Router();
 
-languageRouter.get("/:language", async (req, res) => {
+languageRouter.get("/", async (req, res) => {
   try {
-    const language = req.params.language;
-    const languageInfo = await database.findLanguage(language);
-    const wordCount = await database.getWordCountForLanguage(language);
-
-    const responseData = {
-      language: languageInfo,
-      wordCount: wordCount,
-    };
-
-    res.json(responseData);
+    const languagesWithWordCount =
+      await database.getAllLanguagesWithWordCount();
+    res.json(languagesWithWordCount);
   } catch (err) {
     res.status(500).json({ msg: err });
   }
