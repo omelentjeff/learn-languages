bc278ea4dee860607c532cc8910955725cfa1174
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-07 15:57:11 +0200

    feat: add route to EditLanguage.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import Home from "./components/Home";
 import TeacherDashboard from "./components/TeacherDashboard";
+import EditLanguage from "./components/EditLanguage"; //
+
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -13,8 +15,6 @@ const darkTheme = createTheme({
     mode: "dark",
   },
 });
-
-console.log(darkTheme);
 
 function App() {
   return (
@@ -26,6 +26,7 @@ function App() {
           <Route path="/" element={<SignIn />} />
           <Route path="/home" element={<Home />} />
           <Route path="/teacher" element={<TeacherDashboard />} />
+          <Route path="/edit/:languageId" element={<EditLanguage />} />
         </Routes>
       </Router>
     </ThemeProvider>
