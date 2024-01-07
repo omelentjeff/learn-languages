b77de1b103dce34922bfc45ad9651e820ba954fc
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-07 14:54:30 +0200

    feat: combine language data and word count into same response

--- backend/routes/languageRouter.js
+++ backend/routes/languageRouter.js
@@ -4,9 +4,20 @@ const express = require("express");
 const languageRouter = express.Router();
 
 languageRouter.get("/:language", async (req, res) => {
-  const language = req.params.language;
-  const words = await database.findLanguage(language);
-  res.json(words);
+  try {
+    const language = req.params.language;
+    const languageInfo = await database.findLanguage(language);
+    const wordCount = await database.getWordCountForLanguage(language);
+
+    const responseData = {
+      language: languageInfo,
+      wordCount: wordCount,
+    };
+
+    res.json(responseData);
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 });
 
 module.exports = languageRouter;
