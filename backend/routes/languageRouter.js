e4a8ed9958983edd1409f6b920001ee63eac9722
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-07 14:44:35 +0200

    feat: create route for find language

--- backend/routes/languageRouter.js
+++ backend/routes/languageRouter.js
@@ -3,4 +3,10 @@ const { validateResult } = require("../utils/validation");
 const express = require("express");
 const languageRouter = express.Router();
 
+languageRouter.get("/:language", async (req, res) => {
+  const language = req.params.language;
+  const words = await database.findLanguage(language);
+  res.json(words);
+});
+
 module.exports = languageRouter;
