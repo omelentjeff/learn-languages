7cee9c6c416b4e818052ea68c028a12d83c6b016
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-07 14:20:25 +0200

    feat: create languageRouter.js

---
+++ backend/routes/languageRouter.js
@@ -0,0 +1,6 @@
+const database = require("../database/database");
+const { validateResult } = require("../utils/validation");
+const express = require("express");
+const languageRouter = express.Router();
+
+module.exports = languageRouter;
