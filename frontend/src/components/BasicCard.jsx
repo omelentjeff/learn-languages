cdb72f5110690ebcfbf45b7e1bcf2d382a7b11ce
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-08 11:05:01 +0200

    feat: implement axios request to delete a language

--- frontend/src/components/BasicCard.jsx
+++ frontend/src/components/BasicCard.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
@@ -7,7 +7,43 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  DialogActions,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+
+import axios from "axios";
+
+// ...
+
 export default function BasicCard({ languageId, languageName, wordCount }) {
+  const [openConfirmation, setOpenConfirmation] = useState(false);
+  const [wordIdToDelete, setWordIdToDelete] = useState(null);
+
+  const handleDeleteRow = () => {
+    setWordIdToDelete(languageId);
+    setOpenConfirmation(true);
+  };
+
+  const handleConfirmation = async () => {
+    setOpenConfirmation(false);
+
+    try {
+      await axios.delete(
+        `${import.meta.env.VITE_API_URL}/api/languages/${wordIdToDelete}`
+      );
+
+      setWordIdToDelete(null);
+    } catch (error) {
+      console.error("Error deleting row:", error);
+    }
+  };
+
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
@@ -21,14 +57,35 @@ export default function BasicCard({ languageId, languageName, wordCount }) {
           {languageName}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Excercises: {wordCount}
+          Exercises: {wordCount}
         </Typography>
       </CardContent>
-      <CardActions>
+      <CardActions sx={{ display: "flex", justifyContent: "space-between" }}>
         <Link to={`/edit/${languageName}`}>
           <Button size="small">Edit</Button>
         </Link>
+        <IconButton onClick={handleDeleteRow}>
+          <DeleteIcon />
+        </IconButton>
       </CardActions>
+
+      <Dialog
+        open={openConfirmation}
+        onClose={() => setOpenConfirmation(false)}
+      >
+        <DialogTitle>Delete Exercise</DialogTitle>
+        <DialogContent>
+          <p>Are you sure you want to delete this exercise?</p>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpenConfirmation(false)} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleConfirmation} color="primary">
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Card>
   );
 }
