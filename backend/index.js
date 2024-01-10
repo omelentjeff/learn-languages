50da761514639f88d33483aefb5def8e0c12f5a8
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-10 16:01:29 +0200

    feat: add categoryRouter to index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const wordRouter = require("./routes/wordRouter");
 const userRouter = require("./routes/userRouter");
 const languageRouter = require("./routes/languageRouter");
+const categoryRouter = require("./routes/categoryRouter");
 const port = 8080;
 const app = express();
 const path = require("path");
@@ -20,6 +21,7 @@ app.use(cors());
 app.use("/api/words", wordRouter);
 app.use("/api/languages", languageRouter);
 app.use("/api/users", userRouter);
+app.use("/api/categories", categoryRouter);
 
 // app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
