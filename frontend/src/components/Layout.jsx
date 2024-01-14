d8e40d27fb7eff44011869646458208213167565
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-14 13:46:11 +0200

    feat: create layout component for navbar

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,91 @@
+// Layout.jsx
+
+import React from "react";
+import { styled } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import Link from "@mui/material/Link";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Hidden from "@mui/material/Hidden";
+import Container from "@mui/material/Container";
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: "100%",
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+export default function Layout({ children }) {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="absolute">
+        <Toolbar
+          sx={{
+            pr: "24px",
+          }}
+        >
+          <Typography
+            component="h1"
+            variant="h5"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1, textAlign: "center" }}
+          >
+            Learn Languages!
+          </Typography>
+
+          <IconButton color="inherit">
+            <Hidden smDown>
+              <Typography
+                variant="body1"
+                color="inherit"
+                sx={{ marginRight: 1 }}
+              >
+                Log Out
+              </Typography>
+            </Hidden>
+            <Badge color="secondary">
+              <LogoutIcon />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: "100vh",
+          overflow: "auto",
+        }}
+      >
+        <Toolbar />
+        {/* Content */}
+        {children}
+      </Box>
+    </Box>
+  );
+}
