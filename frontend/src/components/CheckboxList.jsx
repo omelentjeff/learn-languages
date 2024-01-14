a8aee09e9d1e3e679c4995bec203b3351271487f
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-14 11:50:35 +0200

    feat: style checkbox list and add error message for empty list

---
+++ frontend/src/components/CheckboxList.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Checkbox from "@mui/material/Checkbox";
+import axios from "axios";
+
+export default function CheckboxList() {
+  const [checked, setChecked] = useState([0]);
+  const [categoryData, setCategoryData] = useState([]);
+  const { languageName } = useParams();
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/categories/${languageName}`
+      );
+      console.log(response.data);
+      setCategoryData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  return (
+    <div>
+      {categoryData.length === 0 ? (
+        <p>No categories available</p>
+      ) : (
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        >
+          {categoryData.map((category) => {
+            const labelId = category.category_id;
+
+            return (
+              <ListItem key={category.category_id} disablePadding>
+                <ListItemButton
+                  role={undefined}
+                  onClick={handleToggle(category.category_id)}
+                  dense
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={checked.indexOf(category.category_id) !== -1}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ "aria-labelledby": labelId }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText id={labelId} primary={category.category_name} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+      )}
+    </div>
+  );
+}
