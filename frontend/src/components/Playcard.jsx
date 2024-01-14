47d2d18ddead5e1f2ca47dc3be3a9ffda25f3625
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-14 11:01:34 +0200

    feat: implement player cards for player dashboard

---
+++ frontend/src/components/Playcard.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+// ...
+
+export default function PlayCard({ languageId, languageName, wordCount }) {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography
+          component="h2"
+          variant="h4"
+          color="text.secondary"
+          gutterBottom
+          sx={{ fontWeight: "bold" }}
+        >
+          {languageName}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Exercises: {wordCount}
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Link to={`/play/${languageName}`}>
+          <Button size="medium">Play</Button>
+        </Link>
+      </CardActions>
+    </Card>
+  );
+}
