d682b9f40a63560606910c4094ff8243d4b46c11
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-15 18:36:55 +0200

    add wildcard route to catch unhandled client side routes

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,9 @@ app.use("/api/users", userRouter);
 app.use("/api/categories", categoryRouter);
 
 app.use(express.static("./frontend/dist"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./frontend/dist/index.html"));
+});
 
 const server = app
   .listen(port, () => {
