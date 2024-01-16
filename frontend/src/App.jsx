d6bde6614e62d1084263f64e54ee8db587f3e0b2
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-16 16:07:02 +0200

    feat: add dark mode toggle support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-// App.jsx
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
@@ -12,15 +11,34 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import CategorySelector from "./components/CategorySelector";
 
+import Switch from "@mui/material/Switch";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
   },
 });
 
+// TODO: AUTH, PROTECTED ROUTES, LOGOUT, PICK WHICH LANGUAGE TO PLAY, MOBILE FRIENDLY, BETTER UI
+
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+    },
+  });
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
         <Routes>
@@ -39,6 +57,27 @@ function App() {
           <Route path="/play/:language" element={<ExcercisePage />} />
         </Routes>
       </Router>
+
+      <Grid
+        component="label"
+        container
+        alignItems="center"
+        justifyContent="flex-end"
+        style={{ position: "fixed", bottom: 16, right: 16 }}
+      >
+        <Grid item>
+          <Typography color={darkMode ? "textSecondary" : "textPrimary"}>
+            Dark Mode
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Switch
+            checked={darkMode}
+            onChange={toggleDarkMode}
+            color="primary"
+          />
+        </Grid>
+      </Grid>
     </ThemeProvider>
   );
 }
