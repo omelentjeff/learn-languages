615bc262ae0be9db9d9a075a2b97746079c05d92
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-18 13:26:21 +0200

    feat: implement language existense checking

--- frontend/src/components/CategorySelector.jsx
+++ frontend/src/components/CategorySelector.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CheckboxList from "./CheckboxList";
 import Button from "@mui/material/Button";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import Layout from "./Layout";
+import axios from "axios";
 
 const CategorySelector = () => {
   const [selectedCategories, setSelectedCategories] = useState([]);
@@ -23,6 +24,31 @@ const CategorySelector = () => {
       },
     });
   };
+
+  const checkLanguageExists = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/languages/${languageName}`,
+        { withCredentials: true }
+      );
+
+      if (response.status === 200) {
+        console.log("Language exists");
+      }
+    } catch (err) {
+      if (err.response && err.response.status === 404) {
+        navigate("*");
+      } else {
+        console.log("Error checking language existence", err);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (languageName) {
+      checkLanguageExists();
+    }
+  }, [languageName, navigate]);
 
   return (
     <Layout>
