1037ce4b0416626528f34329962df7cf7922d5b1
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-19 20:47:13 +0200

    bug: fix CORS options

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 
 // CORS configuration
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5175",
   credentials: true,
 };
 app.use(cors(corsOptions));
