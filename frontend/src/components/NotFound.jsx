261b98594f6098887f6ccea6f0dd54d51b6f73ec
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-19 11:43:59 +0200

    feat: implement routing based on user role

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,12 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const NotFound = () => (
-  <div>
-    <h1>Ooops! Page not found</h1>
-    <p>The page you're looking for does not exist.</p>
-    <Link to="/home">Go to the Homepage</Link>
-  </div>
-);
+const NotFound = () => {
+  const navigate = useNavigate();
+  const [userRole, setUserRole] = useState(null);
+
+  useEffect(() => {
+    const fetchRole = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/api/users/role`,
+          { withCredentials: true }
+        );
+        setUserRole(response.data.role);
+      } catch (error) {
+        console.error("Error or unauthorized", error);
+        navigate("/");
+      }
+    };
+
+    fetchRole();
+  }, [navigate]);
+
+  const handleButton = () => {
+    if (userRole === "teacher") {
+      navigate("/teacher");
+    } else {
+      navigate("/home");
+    }
+  };
+  return (
+    <div>
+      <h1>Ooops! Page not found</h1>
+      <p>The page you're looking for does not exist.</p>
+      <button onClick={handleButton}>Go to home page</button>
+    </div>
+  );
+};
 
 export default NotFound;
--- frontend/src/components/Unauthorized.jsx
+++ frontend/src/components/Unauthorized.jsx
@@ -1,12 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const Unauthorized = () => (
-  <div>
-    <h1>Unauthorized Access</h1>
-    <p>You do not have permission to view this page.</p>
-    <Link to="/home">Go to the Homepage</Link>
-  </div>
-);
+const Unauthorized = () => {
+  const navigate = useNavigate();
+  const [userRole, setUserRole] = useState(null);
+
+  useEffect(() => {
+    const fetchRole = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/api/users/role`,
+          { withCredentials: true }
+        );
+        setUserRole(response.data.role);
+      } catch (error) {
+        console.error("Error or unauthorized", error);
+        navigate("/");
+      }
+    };
+
+    fetchRole();
+  }, [navigate]);
+
+  const handleButton = () => {
+    if (userRole === "teacher") {
+      navigate("/teacher");
+    } else {
+      navigate("/home");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Unauthorized Access</h1>
+      <p>You do not have permission to view this page.</p>
+      <button onClick={handleButton}>Go to home page</button>
+    </div>
+  );
+};
 
 export default Unauthorized;
