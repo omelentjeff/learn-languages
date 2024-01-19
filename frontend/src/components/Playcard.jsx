30ff9260aa5becc77a162c74e98f000c36142e5e
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-01-19 12:55:01 +0200

    feat: styling for PlayCard

--- frontend/src/components/Playcard.jsx
+++ frontend/src/components/Playcard.jsx
@@ -1,33 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-
-// ...
+import Box from "@mui/material/Box"; // Import the Box component
 
 export default function PlayCard({ languageId, languageName, wordCount }) {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ minWidth: 275, paddingBottom: 2 }}>
       <CardContent>
-        <Typography
-          component="h2"
-          variant="h4"
-          color="text.secondary"
-          gutterBottom
-          sx={{ fontWeight: "bold" }}
-        >
-          {languageName}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Exercises: {wordCount}
-        </Typography>
+        <Box sx={{ textAlign: "center" }}>
+          {" "}
+          <Typography
+            component="h2"
+            variant="h4"
+            color="text.secondary"
+            gutterBottom
+            sx={{ fontWeight: "bold" }}
+          >
+            {languageName}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            Exercises: {wordCount}
+          </Typography>
+        </Box>
       </CardContent>
-      <CardActions sx={{ display: "flex", justifyContent: "space-between" }}>
+      <CardActions sx={{ display: "flex", justifyContent: "center" }}>
         <Link to={`/${languageName}`}>
-          <Button size="medium">Play</Button>
+          <Button variant="contained" color="primary">
+            PLAY
+          </Button>
         </Link>
       </CardActions>
     </Card>
